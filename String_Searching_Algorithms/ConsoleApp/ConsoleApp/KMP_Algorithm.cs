489067cfe41b24137bc68fdc7f91d75b7b21d6f5
Subin <subinbs4a@gmail.com>
2020-01-04 14:06:20 +0300

    String Search Algorithms

---
+++ String_Searching_Algorithms/ConsoleApp/ConsoleApp/KMP_Algorithm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class KMP_Algorithm
+    {
+
+        public void Start()
+        {
+            bool result = kmpalgo("abcxabcdabcdabcy", "abcdabcy");
+
+            if(result)
+                Console.WriteLine("TRUE");
+            else
+                Console.WriteLine("FALSE");
+        }
+
+        public int[] ComputePrefixTable(string pattern)
+        {
+            int[] table = new int[pattern.Length];
+
+            int j = 0, i = j + 1;
+            table[0] = j;
+
+            while(i < pattern.Length)
+            {
+                if (pattern[i] == pattern[j])
+                {
+                    table[i] = j + 1;
+                    i++;j++;
+                }                    
+                else
+                {
+                    if (j != 0)
+                        j = table[j - 1]; //If mismatch occurs then consider the j value as
+                                          // value of previous location where mismatch occurs.     
+                    else
+                    {
+                        table[i] = 0;
+                        i++;
+                    }
+                }
+            }
+
+            //for (int k = 0; k < table.Length; k++)
+            //{
+            //    Console.WriteLine($"k[{k}] is { table[k] }");
+            //}
+
+            return table;
+        }
+
+        public bool kmpalgo(string text,string pattern)
+        {
+            int[] lps = ComputePrefixTable(pattern);
+            int i = 0, j = 0;
+
+            while(i < text.Length && j < pattern.Length)
+            {
+                if(text[i].Equals(pattern[j]))
+                {
+                    i++;j++;
+                }
+                else
+                {
+                    if (j != 0)
+                        j = lps[j - 1];
+                    else
+                    {
+                        i++; //This will start comparing text with first char of pattern
+                    }
+                }
+            }
+
+            if (j == pattern.Length)
+                return true;
+
+            return false;
+
+        }
+    }
+}
---
+++ String_Searching_Algorithms/ConsoleApp/ConsoleApp/RabinKarpAlgo.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class RabinKarpAlgo
+    {
+        const int hbase = 256;
+        const int prime = 101;
+
+        public void AlgoSteps()
+        {
+            string subStr = string.Empty;
+            bool rollingHash = false;
+            double firstCharValue = 0.0;
+            double textHashValue = 0.0;
+            double patternHashValue = 0.0;
+
+            string pattern = "bra";//hia
+            string text = "abra";
+
+            //Calculate Pattern Hash
+            patternHashValue = ComputeHash(pattern);
+
+            for (int i = 0; i <= text.Length - pattern.Length; i++)
+            {
+                subStr = text.Substring(i, pattern.Length);               
+
+                if (!rollingHash)
+                {
+                    textHashValue = ComputeHash(subStr);
+                    rollingHash = true;
+                }                    
+                else
+                    textHashValue = ComputeRollingHash(textHashValue, subStr[pattern.Length - 1], firstCharValue);
+
+                //Store the Value of first Char for Computing Rolling Hash
+                firstCharValue = ((int)text[i] * (Math.Pow(hbase, (double)pattern.Length - 1 - i))) % prime;
+
+                if (textHashValue == patternHashValue)
+                {
+                    int j = 0;
+                    while (j < pattern.Length)
+                    {
+                        if(subStr[j] == pattern[j])
+                        {
+                            j++;
+                        }
+                    }
+
+                    if(j == pattern.Length)
+                        Console.WriteLine($"Pattern Found at position { i }");
+                }
+            }
+        }
+
+        public double ComputeHash(string str)
+        {
+            int strLength = str.Length;
+            double sum = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                sum += ( (int)str[i] * ( Math.Pow(hbase, (double)strLength - 1 - i) ) ) % prime;
+            }
+
+            return sum % prime;
+        }
+
+        public double ComputeRollingHash(double previousHashValue,char nextChar,double previousFirstCharComputedValue)
+        {
+            double nextHashValue = 0.0;
+
+            nextHashValue = ( ((previousHashValue + (prime - previousFirstCharComputedValue)) * hbase) +
+                (int)nextChar) % prime;
+
+            return nextHashValue;
+        }
+
+
+    }
+}
